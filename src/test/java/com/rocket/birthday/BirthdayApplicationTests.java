240ef39918b56b588a714bced90392d3c610db8a
kimdoha <kkhhjj3393@gmail.com>
2023-06-26 10:58:59 +0900

    feat: git init

---
+++ src/test/java/com/rocket/birthday/BirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.birthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
