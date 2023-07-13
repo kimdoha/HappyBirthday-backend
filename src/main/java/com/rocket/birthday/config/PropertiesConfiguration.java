c1a0f032619f5246418a4475f0c36c3f013fa8bf
kimdoha <kkhhjj3393@gmail.com>
2023-07-13 15:34:23 +0900

    feat: - ConfigurationPropertiesScan을 통한 프로퍼티 주입 (#2)
    -

---
+++ src/main/java/com/rocket/birthday/config/PropertiesConfiguration.java
@@ -0,0 +1,14 @@
+package com.rocket.birthday.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "kakao")
+public class PropertiesConfiguration {
+  private String client_id;
+  private String redirect_uri;
+  private String grant_type;
+}
