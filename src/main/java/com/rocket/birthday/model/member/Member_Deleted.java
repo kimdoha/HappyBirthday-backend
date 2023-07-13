59877e4fb491075900f0ed95ce00800e2856346f
kimdoha <kkhhjj3393@gmail.com>
2023-07-13 17:36:20 +0900

    feat: 탈퇴유저를 관리하는 Entity 수정 (#2)

--- src/main/java/com/rocket/birthday/model/member/Member_Deleted.java
+++ src/main/java/com/rocket/birthday/model/member/Member_Deleted.java
@@ -1,35 +1,56 @@
 package com.rocket.birthday.model.member;
 
+import com.rocket.birthday.model.member.vo.ProviderType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+    name = "members_deleted",
+    uniqueConstraints = @UniqueConstraint(
+        name = "member_deleted_unique_columns",
+        columnNames = { "provider_id", "provider_type", "email", "name"}))
+@Entity
 public class Member_Deleted {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false, unique = true)
-  private Long providerId;
+  @Column(name = "provider_id")
+  private String providerId;
 
-  @Column(nullable = false, unique = true)
+  @Column(name = "provider_type")
+  @Enumerated(EnumType.STRING)
+  private ProviderType providerType;
+
+  @Column(nullable = false)
   private String email;
-
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false)
   private String name;
-
-  @Column()
+  @Column(name = "profile_image_url")
   private String profileImageUrl;
 
   @Column(nullable = false)
   private LocalDate birthday;
 
   @CreatedDate
-  @Column(updatable = false)
+  @Column(name = "deleted_at", updatable = false)
   private ZonedDateTime deletedAt;
 }
