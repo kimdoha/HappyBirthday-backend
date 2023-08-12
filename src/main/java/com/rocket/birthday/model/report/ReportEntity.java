d78a01d8a025c6b5cb1d4f65da6eecf1c287f7c1
kimdoha <kkhhjj3393@gmail.com>
2023-08-12 09:29:44 +0900

    feat: ReportEntity 생성(#9)

---
+++ src/main/java/com/rocket/birthday/model/report/ReportEntity.java
@@ -0,0 +1,55 @@
+package com.rocket.birthday.model.report;
+
+import static com.rocket.birthday.common.constant.BirthdayConstants.SEOUL_ZONEID;
+
+import com.rocket.birthday.model.member.Member;
+import com.rocket.birthday.model.message.Message;
+import com.rocket.birthday.model.report.vo.ReportType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reports")
+@Entity
+public class ReportEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(value = EnumType.STRING)
+  @Column(name = "report_type", nullable = false)
+  private ReportType reportType;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "message_id", referencedColumnName = "id")
+  private Message message;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", referencedColumnName = "id")
+  private Member member;
+
+  private ZonedDateTime createdAt;
+
+  @PrePersist
+  public void onPrePersist() {
+    this.createdAt = ZonedDateTime.now(SEOUL_ZONEID);
+  }
+}
